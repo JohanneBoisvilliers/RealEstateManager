d1b8dfb347ac4d8f7713e0f488429dd683e67fb9
Johanne Boisvilliers <johanne.boisvilliers@gmail.com>
2019-07-21 15:56:20 +0200

    add progressbar on sign in button and a snackbar to show authentication fail

--- app/src/main/java/com/openclassrooms/realestatemanager/login/SignInActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/login/SignInActivity.java
@@ -2,16 +2,18 @@ package com.openclassrooms.realestatemanager.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.BaseTransientBottomBar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ProgressBar;
 
 import com.openclassrooms.realestatemanager.R;
 import com.openclassrooms.realestatemanager.controllers.MainActivity;
 import com.openclassrooms.realestatemanager.utils.AgentAsyncTask;
+import com.openclassrooms.realestatemanager.utils.Utils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,6 +26,10 @@ public class SignInActivity extends AppCompatActivity implements AgentAsyncTask.
     TextInputEditText mPassword;
     @BindView(R.id.signin_btn)
     Button mSignInButton;
+    @BindView(R.id.signin_progress_bar)
+    ProgressBar mProgressBar;
+    @BindView(R.id.coordinator_sign_in)
+    View mCoordinator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,22 +55,32 @@ public class SignInActivity extends AppCompatActivity implements AgentAsyncTask.
         new AgentAsyncTask(this, mUsername.getText().toString(),
                 mPassword.getText().toString()).execute();
     }
-
     @Override
     public void onPreExecute() {
+        this.updateUIBeforeTask();
     }
-
     @Override
     public void doInBackground() {
-
     }
-
     @Override
     public void onPostExecute(Long success) {
+        this.updateUIAfterTask();
         if (success == 1) {
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
         } else {
-            Toast.makeText(getApplicationContext(), "ERREUR", Toast.LENGTH_LONG).show();
+            Utils.showSnackBar(mCoordinator, getString(R.string.error), BaseTransientBottomBar.LENGTH_LONG);
         }
+    }
+
+    // ---UI---
+
+    public void updateUIBeforeTask() {
+        mProgressBar.setVisibility(View.VISIBLE);
+        mSignInButton.setVisibility(View.GONE);
+    }
+
+    public void updateUIAfterTask() {
+        mProgressBar.setVisibility(View.GONE);
+        mSignInButton.setVisibility(View.VISIBLE);
     }
 }
